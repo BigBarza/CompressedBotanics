10fad8ffca1ff773e9762d81e12c64c6d3fd1681
Big Barza <francesco.b99@hotmail.it>
2024-07-22 22:25:05 +0200

    Reworked tooltips -Moved strings to lang -Reworked function of most tooltips

--- src/main/java/com/pression/compressedbotanics/compat/jei/FloralEntropyRecipeCategory.java
+++ src/main/java/com/pression/compressedbotanics/compat/jei/FloralEntropyRecipeCategory.java
@@ -24,12 +24,12 @@ import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
 import org.jetbrains.annotations.NotNull;
 import vazkii.botania.client.gui.HUDHandler;
+import vazkii.botania.client.integration.shared.OrechidUIHelper;
 import vazkii.botania.common.block.block_entity.mana.ManaPoolBlockEntity;
 import vazkii.botania.common.item.BotaniaItems;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 //This handles the rendering of the recipe in JEI
@@ -47,7 +47,7 @@ public class FloralEntropyRecipeCategory implements IRecipeCategory<FloralEntrop
     private final ResourceLocation texPath = new ResourceLocation(CompressedBotanics.MODID, "textures/gui/jei.png");
 
     public FloralEntropyRecipeCategory(IGuiHelper guiHelper){
-        this.title = Component.literal("Floral Entropy");
+        this.title = Component.translatable("compressedbotanics.jei.title");
         this.background = guiHelper.createBlankDrawable(169, 58);
         this.overlay = guiHelper.createDrawable(texPath, 0,0, 91 ,48);
         this.zoomLens = guiHelper.createDrawable(texPath, 91,3, 7 ,7);
@@ -96,7 +96,7 @@ public class FloralEntropyRecipeCategory implements IRecipeCategory<FloralEntrop
         if(recipe.getMinTalliedMana() > 0) { //If no mana is required, don't draw the mana bar.
             String zoomFactor = getZoomFactor(recipe.getMinTalliedMana());
             HUDHandler.renderManaBar(ms, 55, 50, 0x0000FF, 0.75F, recipe.getMinTalliedMana(), (int) (ManaPoolBlockEntity.MAX_MANA / Float.parseFloat(zoomFactor))); //NOTE: This method always creates a mana bar 102 units wide and 5 units tall.
-            Component zoomText = Component.literal("x" + zoomFactor); //Whenever the overlay is reimplemented, add a little magnifying glass icon.
+            Component zoomText = Component.literal("x" + zoomFactor);
             Font font = Minecraft.getInstance().font;
             font.draw(ms, zoomText, 24, 47, 0x888888);
             zoomLens.draw(ms, 13, 47);
@@ -120,10 +120,10 @@ public class FloralEntropyRecipeCategory implements IRecipeCategory<FloralEntrop
 
         if(fluid.isEmpty()){
             builder.addSlot(RecipeIngredientRole.OUTPUT, xOffset+66, 28).addItemStack(decayed)
-                    .addTooltipCallback((recipeSlotView, tooltip) -> tooltip.add(1, Component.literal("Left over after entropic decay.")));
+                    .addTooltipCallback((recipeSlotView, tooltip) -> tooltip.add(1, Component.translatable("compressedbotanics.jei.block")));
         }else{
             builder.addSlot(RecipeIngredientRole.OUTPUT, xOffset+66, 28).addFluidStack(fluid.getFluid(), 1000)
-                    .addTooltipCallback((recipeSlotView, tooltip) -> tooltip.add(1, Component.literal("Left over after entropic decay.")));
+                    .addTooltipCallback((recipeSlotView, tooltip) -> tooltip.add(1, Component.translatable("compressedbotanics.jei.block")));
         }
 
         xOffset += overlay.getWidth();
@@ -155,23 +155,35 @@ public class FloralEntropyRecipeCategory implements IRecipeCategory<FloralEntrop
     @NotNull
     @Override
     public List<Component> getTooltipStrings(FloralEntropyRecipe recipe, IRecipeSlotsView recipeSlotsView, double mouseX, double mouseY){
-        if(mouseY >= 50){ //TODO: Pad this a bit more.
-            List<Component> tooltips = new ArrayList<>();
-            if(recipe.getMinTalliedMana() > 0){ //There's no mana bar in this case.
-                tooltips.add(Component.literal("Must produce at least "+(recipe.getMinTalliedMana()<1000 ? "a small amount" : ((double) recipe.getMinTalliedMana()/ManaPoolBlockEntity.MAX_MANA+" Mana Pool(s)' worth"))+" of mana before entropic decay is possible."));
-                //That thing right above? It's ugly, i know. But it's hard to get that number to render semi-properly.
+        List<Component> tooltips = new ArrayList<>();
+        if(recipe.getMinTalliedMana() > 0 && mouseY >= 49){ //Mana bar tooltip
+            if(mouseX >= 13 && mouseX <= 52){ //Zoom factor
+                tooltips.add(Component.translatable("compressedbotanics.jei.zoom"));
             }
-            return tooltips;
+            if(mouseX >= 54 && mouseX <= 156){ //The bar itself
+                double mana = recipe.getMinTalliedMana(); //Casting to double to allow non-int division
+                double pools = mana >= 100000 ? mana / 1000000 : mana / 10000;
+                tooltips.add(Component.translatable("compressedbotanics.jei.manabar_1")
+                        .append(String.format("%.2f", pools)) //We...don't need more than two decimals. Do we? I mean, easy to tweak.
+                        .append(Component.translatable(mana >= 100000 ? "" : "compressedbotanics.jei.manabar_2"))
+                        .append(Component.translatable("compressedbotanics.jei.manabar_3")));
+
+            }
         }
-        return Collections.emptyList();
+        int xOffset = getOffset(recipe.getResult());
+        if(hasEnchanted(recipe.getResult()) && mouseX >= xOffset+27 && mouseX <= xOffset+44 && mouseY >= 2 && mouseY <= 19){
+            tooltips.add(Component.translatable("compressedbotanics.jei.enchanted_warning_1"));
+            tooltips.add(Component.translatable("compressedbotanics.jei.enchanted_warning_2"));
+        }
+        return tooltips;
     }
 
-    //TODO: Move this somewhere else in the renderer. Maybe to the right of the mana bar?
+
     private Collection<Component> getInputTooltip(FloralEntropyRecipe recipe){
         List<Component> tooltips = new ArrayList<>();
-        tooltips.add(Component.literal("Minimum time: "+recipe.getMinDecayTicks()+" ticks"));
-        tooltips.add(Component.literal("Maximum time: "+recipe.getMaxDecayTicks()+" ticks"));
-        tooltips.add(Component.literal("Chance of entropic decay scales between the two time intervals"));
+        tooltips.add(Component.translatable("compressedbotanics.jei.input_min").append(getFormattedTime(recipe.getMinDecayTicks())));
+        tooltips.add(Component.translatable("compressedbotanics.jei.input_max").append(getFormattedTime(recipe.getMaxDecayTicks())));
+        tooltips.add(Component.translatable("compressedbotanics.jei.input_info"));
         return tooltips;
     }
     private ItemStack prepareItemNBT(ChanceOutput output){
@@ -187,9 +199,10 @@ public class FloralEntropyRecipeCategory implements IRecipeCategory<FloralEntrop
     private Collection<Component> getOutputTooltip(ItemStack output){
         List<Component> tooltips = new ArrayList<>();
         CompoundTag nbt = output.getTag();
-        tooltips.add(Component.literal("Chance: "+(nbt.getFloat("chance"))));
-        tooltips.add(Component.literal(nbt.getBoolean("allornothing") ? "All or nothing" : "Individual rolls"));
-        if(nbt.getBoolean("enchanted")) tooltips.add(Component.literal("Enchanted soil required"));
+        tooltips.add(nbt.getFloat("chance") == 1 ? Component.translatable("compressedbotanics.jei.output_guaranteed") :
+                Component.translatable("compressedbotanics.jei.output_chance").append(OrechidUIHelper.getPercentageComponent(nbt.getFloat("chance"))));
+        if(nbt.getFloat("chance") < 1) tooltips.add(Component.translatable(nbt.getBoolean("allornothing") ? "compressedbotanics.jei.output_aon" : "compressedbotanics.jei.output_normal")); //Don't display roll type if guaranteed.
+        if(nbt.getBoolean("enchanted")) tooltips.add(Component.translatable("compressedbotanics.jei.output_enchanted"));
         return tooltips;
     }
 
@@ -199,6 +212,16 @@ public class FloralEntropyRecipeCategory implements IRecipeCategory<FloralEntrop
         if(amount < 10000) return "100"; //Less than one hundredth of a mana pool. I guess it's ok for stuff like the hydroangeas.
         if(amount < 100000) return "10.0"; //Between one hundredth and one tenth of a mana pool.
         return "1"; //between one tenth of a pool and one full pool.
+    }
+
+    private String getFormattedTime(int ticks){
+        int totalSeconds = ticks / 20;
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+        String output = (hours > 0 ? hours+"h " : "")+(minutes > 0 ? minutes+"m " : "")+(seconds > 0 ? seconds+"s " : "");
+        return output.equals("") ? "-" : output;
+
     }
 
 }
