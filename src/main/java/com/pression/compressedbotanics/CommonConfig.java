af40c72038755aac1d164ce5a2f0f70c42ec4dbb
Big Barza <francesco.b99@hotmail.it>
2025-06-03 22:14:58 +0200

    Added configs for the Cocoon of Caprice

--- src/main/java/com/pression/compressedbotanics/CommonConfig.java
+++ src/main/java/com/pression/compressedbotanics/CommonConfig.java
@@ -9,6 +9,10 @@ public class CommonConfig {
     public static final ForgeConfigSpec.ConfigValue<Boolean> ELYTRA_MANA_REPAIR;
     public static final ForgeConfigSpec.ConfigValue<Integer> ELYTRA_MANA_DAMAGE_COST;
     public static final ForgeConfigSpec.ConfigValue<Integer> ELYTRA_MANA_REPAIR_COST;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> COCOON_NO_EMERALD;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> COCOON_NO_CHORUS;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> COCOON_NO_GAIA;
+    public static final ForgeConfigSpec.ConfigValue<Double> COCOON_RARE_CHANCE;
 
     static {
         BUILDER.push("Compressed Botanics Config");
@@ -19,6 +23,14 @@ public class CommonConfig {
                         .define("Elytra Damage Negation Cost", 100);
         ELYTRA_MANA_REPAIR_COST = BUILDER.comment("How much mana should the elytra draw to repair a previously lost point of durability. Manasteel armor is 140.")
                 .define("Elytra Damage Repair Cost", 200);
+        COCOON_NO_EMERALD = BUILDER.comment("Whether to block emeralds from being used on the Cocoon of Caprice, making it unable to spawn villagers")
+                        .define("No Villagers from Cocoon", false);
+        COCOON_NO_CHORUS = BUILDER.comment("Whether to block chorus fruits from being used on the Cocoon of Caprice, making it unable to spawn shulkers")
+                .define("No Villagers from Cocoon", false);
+        COCOON_NO_GAIA = BUILDER.comment("Whether to block gaia spirits from being used on the Cocoon of Caprice, forcing players to use the normal rare mob chances")
+                .define("No guaranteed rares from Cocoon", false);
+        COCOON_RARE_CHANCE = BUILDER.comment("The chance that a Cocoon of Caprice spawns from the rare list instead of the common list. Default is 0.075 or 7.5%")
+                .defineInRange("Cocoon Rare Chance", 0.075, 0,1);
 
         BUILDER.pop();
         SPEC = BUILDER.build();
---
+++ src/main/java/com/pression/compressedbotanics/mixin/CocoonChanceMixin.java
@@ -0,0 +1,16 @@
+package com.pression.compressedbotanics.mixin;
+
+import com.pression.compressedbotanics.CommonConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import vazkii.botania.common.block.block_entity.CocoonBlockEntity;
+
+@Mixin(CocoonBlockEntity.class)
+public class CocoonChanceMixin {
+    @Redirect(method = "hatch", at = @At(value = "INVOKE", target = "Ljava/lang/Math;random()D"), remap = false)
+    private double setNewChance(){
+        if(Math.random() < CommonConfig.COCOON_RARE_CHANCE.get()) return 0;
+        else return 1;
+    }
+}
